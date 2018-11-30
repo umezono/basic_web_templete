7333b8b8e72283250679b2dcac7b5644222fcb4c
umezono <umezono@imgsrc.co.jp>
2018-11-30 16:57:00 +0900

    uriの追加

--- configs/setting.js
+++ configs/setting.js
@@ -13,27 +13,66 @@ export default class Setting {
       assets: 'assets',
       css: 'css',
       js: 'js'
-    }
+    };
 
     this.localDir = {
       dir: 'dist-local',
       assets: 'assets',
       css: 'css',
       js: 'js'
-    }
+    };
 
     this.developmentDir = {
       dir: 'dist-development',
       assets: 'assets',
       css: 'css',
       js: 'js'
-    }
+    };
 
     this.productionDir = {
       dir: 'dist-production',
       assets: 'assets',
       css: 'css',
       js: 'js'
+    };
+
+    /**
+    * Location
+    */
+    if (process.env.NODE_ENV == 'local') {
+
+      this.location = {
+        protocol: 'http:',
+        hostname: '//127.0.0.1',
+        port: ':6000'
+      };
+
+      this.location.host = `${this.location.protocol}${this.location.hostname}${this.location.port}`;
+
+      this.location.root = `${this.location.protocol}${this.location.hostname}`;
+
+    } else if (process.env.NODE_ENV == 'development') {
+
+      this.location = {
+        protocol: 'https:',
+        hostname: '//hogehoge.com',
+        port: ':8000'
+      };
+
+      this.location.host = `${this.location.protocol}${this.location.hostname}${this.location.port}`;
+
+      this.location.root = `${this.location.protocol}${this.location.hostname}`;
+
+    } else if (process.env.NODE_ENV == 'production') {
+      this.location = {
+        protocol: 'https:',
+        hostname: '//fugafuga.com',
+        port: ''
+      };
+
+      this.location.host = `${this.location.protocol}${this.location.hostname}${this.location.port}`;
+
+      this.location.root = `${this.location.protocol}${this.location.hostname}`;
     }
   };
 
