5b65fb6610c67b679d180513e1bccc6093609e60
umezono <takashi.umezono@gmail.com>
2018-12-02 18:57:23 +0900

    nodeで実行できるように修正

--- tasks/browsersync.js
+++ tasks/browsersync.js
@@ -1,9 +1,12 @@
 'use strict';
 
-import browserSync from 'browser-sync';
-import path from 'path';
+// import browserSync from 'browser-sync';
+// import path from 'path';
 
-const bs = browserSync.create();
+// const bs = browserSync.create();
+
+const bs = require("browser-sync").create();
+const path = require("path");
 
 bs.watch(`${process.cwd()}/dist-local/**`).on("change", bs.reload);
 
@@ -11,7 +14,7 @@ bs.init({
   // https: true,
   port: 3000,
   server: `${process.cwd()}/dist-local`,
-  files: `${process.cwd()}/dist-local/**/*`
-  // files: path.join(`${process.cwd()}/dist-local`, '/**/+(*.html|*.js|*.css)')
+  // files: `${process.cwd()}/dist-local/**/*`
+  files: path.join(`${process.cwd()}/dist-local`, '/**/+(*.html|*.js|*.css)')
   // "browser": ["firefox", "chrome"],
 });
