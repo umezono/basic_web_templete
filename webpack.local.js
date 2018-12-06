248d6a03e816aec55c2c729c27ab3713fe884a05
umezono <takashi.umezono@gmail.com>
2018-12-07 01:05:53 +0900

    watchをコメントアウト

--- webpack.local.js
+++ webpack.local.js
@@ -2,7 +2,7 @@ const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 
 module.exports = merge(commonConfig, {
-  watch: 'true',
+  // watch: 'true',
   mode: 'development',
   devtool: 'source-map',
   output: {
