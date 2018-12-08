6e0691907b17e31348b512a3f8b706753855e309
umezono <takashi.umezono@gmail.com>
2018-12-09 01:14:44 +0900

    ファイル、フォルダの監視を追加

--- tasks/ejs.js
+++ tasks/ejs.js
@@ -4,6 +4,7 @@ const fs = require('fs-extra');
 
 const ejs = require('ejs');
 
+const chokidar = require('chokidar');
 const moment = require('moment');
 const chalk = require('chalk');
 
@@ -19,11 +20,18 @@ const options = {
   delimiter: '@'
 };
 
+const meta = new Meta();
+const setting = new Setting();
+
 const data = {
   rootPath: process.cwd(),
-  meta: new Meta(),
-  setting: new Setting()
+  meta: meta,
+  setting: setting
 };
+
+const ejsFiles = util.getSyncFiles('src/**/*.ejs', 'src/**/_*.ejs');
+
+let distDir;
 
 const ejsCompile = (file, data, options) => {
   return new Promise((resolve, reject) => {
@@ -38,36 +46,91 @@ const ejsCompile = (file, data, options) => {
   });
 }
 
-(async () => {
+const readyComplie = () => {
+  try {
+    let startTime = moment();
 
-    const setting = new Setting();
+    Promise.all(ejsFiles.map(
+      async (file) => {
+        let dist = await util.getDistPath(file, distDir).replace(`/${setting.srcDir.ejs}`, '');
 
-    let ejsFiles = util.getSyncFiles('src/**/*.ejs', 'src/**/_*.ejs');
+        await util.mkdirpSync(dist);
 
-    try{
-      if (process.env.NODE_ENV == 'local') {
+        let renderHTML = await ejsCompile(file, data, options);
 
-        ejsFiles.forEach(async (file) => {
-          let dist = await util.getDistPath(file, `${setting.localDir.dir}`).replace('/view', '');
+        await util.writeFileSync(file.replace(`${setting.srcDir.dir}/${setting.srcDir.ejs}`, distDir).replace('ejs', 'html'), renderHTML);
+      })
+    ).then(() => {
+      console.log(chalk.white.bold.bgMagenta(`ejs compile ${moment().diff(startTime)}ms`));
+    });
+  } catch (error) {
+    util.handleError('ejs compile', error);
+  }
+}
 
-          await util.mkdirpSync(dist);
+(() => {
 
-          let renderHTML = await ejsCompile(file, data, options);
-
-          await util.writeFileSync(file.replace('src/view', `${setting.localDir.dir}`).replace('ejs', 'html'), renderHTML);
-        });
-
-      } else if ((process.env.NODE_ENV == 'development')) {
-
-      } else if ((process.env.NODE_ENV == 'production')) {
-
-      } else {
-        console.log(chalk.white.bold.bgRed(`ejs not NODE_ENV`));
-      }
-    } catch (error) {
-      util.handleError('ejs compile', error);
+    if (process.env.NODE_ENV == 'local') {
+      distDir = `${setting.localDir.dir}`;
+    } else if ((process.env.NODE_ENV == 'development')) {
+      distDir = `${setting.devDir.dir}`;
+    } else if ((process.env.NODE_ENV == 'production')) {
+      distDir = `${setting.prodDir.dir}`;
+    } else {
+      console.log(chalk.white.bold.bgRed(`ejs not NODE_ENV`));
     }
 
-    console.log(chalk.white.bold.bgBlue(`ejs compile time ${moment().format('YYYY-MM-DD HH:mm:ss')}`));
+    const watcher = chokidar.watch(`${setting.srcDir.dir}/${setting.srcDir.ejs}`, {
+      ignored: /.DS_Store/
+    });
+
+    // watch開始時の処理
+    watcher.on('ready', () => {
+      console.log(chalk.white.bold.bgBlue('ejs folder watching...'));
+
+      readyComplie();
+
+      // ファイル変更時の処理
+      watcher.on('change', async (path) => {
+        let searchString = 'common';
+        let searchCommon = path.indexOf(searchString);
+
+        // commonファイルが変更された場合は全てのejsファイルをコンパイル
+        if (searchCommon != -1) {
+          readyComplie();
+
+          console.log(chalk.white.bold.bgBlue('change common file, all ejs file compile'));
+        } else {
+          let renderHTML = await ejsCompile(path, data, options);
+
+          await util.writeFileSync(path.replace(`${setting.srcDir.dir}/${setting.srcDir.ejs}`, distDir).replace('ejs', 'html'), renderHTML);
+
+          console.log(chalk.white.bold.bgBlue(`${path} file compile`));
+        }
+      });
+
+      // ファイルの追加、削除時の処理
+      watcher.on('add', (path) => {
+        watcher.add(path);
+        console.log(chalk.white.bold.bgBlue(`watcher add file ${path}`));
+      }).on('unlink', (path) => {
+        watcher.unwatch(path);
+        console.log(chalk.white.bold.bgBlue(`watcher unlink file ${path}`));
+      });
+
+      // フォルダの追加、削除時の処理
+      watcher.on('addDir', (path) => {
+        watcher.add(path);
+        console.log(chalk.white.bold.bgBlue(`watcher add folder ${path}`));
+      }).on('unlinkDir', (path) => {
+        watcher.unwatch(path);
+        console.log(chalk.white.bold.bgBlue(`watcher unlink folder ${path}`));
+      });
+
+      // watcher error
+      watcher.on('error', (error) => {
+        console.log(chalk.white.bold.bgRed(`ejs watcher ${error}`));
+      });
+    });
 
 })();
